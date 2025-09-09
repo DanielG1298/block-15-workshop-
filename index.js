ebd46004c20f852faa6b9bb63ea43db59d7084d6
DanielG1298 <glaser.daniel1204@gmail.com>
2025-09-08 22:28:45 -0500

    finished userprompt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 window.onload = function(flavors) {
     let userInput = prompt("enter flavors");
-    
     console.log(userInput);
+    let inputArray = userInput.split(",");
+    console.log(inputArray);
 };
