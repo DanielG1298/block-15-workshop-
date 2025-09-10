a16f9a2d22051955ca698d61016fbd8593b626c3
DanielG1298 <glaser.daniel1204@gmail.com>
2025-09-09 20:49:17 -0500

    made changes

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-window.onload = function(flavors) {
-    let userInput = prompt("enter flavors");
-    console.log(userInput);
-    let inputArray = userInput.split(",");
-    console.log(inputArray);
+// prompt user to eneter froyo flavors upon loading the page
+window.onload = function(froyoFlavors) {
+    let flavorTotal = prompt("enter flavors");
+    console.log(flavorTotal);
+    let arr = flavorTotal.split(",");
+    console.log(arr);
 };
+
+let arr = 0;
+for (arr = 0; arr < flavorTotal.length; arr++) {
+    console.log(flavors[arr]);
+} 
+
+let flavorTotal = {
+    "coffee": 0,
+    "vanilla": 0,
+    "strawberry": 0,
+}
+for (flavorTotal in flavors) {
+    console.log(Object.value["vanilla"])
+};
+
