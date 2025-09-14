106dc47ad4ac8189ffd0309e051f8e0aff0e836d
DanielG1298 <glaser.daniel1204@gmail.com>
2025-09-14 18:11:43 -0500

    website take user input properly now, I hope

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 // prompt user to eneter froyo flavors upon loading the page
-window.onload = function() {
     let input = prompt("enter flavor");
     let array = input.split(',');
-    let flavorTotal = {
-    "coffee": 0,
-    "vanilla": 0,
-    "strawberry": 0,
-};
+    let flavorTotal ={}
+;
 // count froyo flavors 
 for (let i = 0; i < array.length; i++) {
     let flavor = array[i];
     if (flavorTotal.hasOwnProperty(flavor)){
-        flavorTotal[flavor] += 1;
-        console.log(flavorTotal);
+        flavorTotal[flavor] += 1;}
+    else{
+            flavorTotal[flavor] = 1;
+        }
+
+        
     }
-}   
+   
 console.log(flavorTotal);
-}
+
  function printFlavorReport(flavorTotal){
     for (let flavor in flavorTotal){
-        console.log(`$flavor: ${flavorTotal[flavor]}`);
+        console.log(`${flavor}: ${flavorTotal[flavor]}`);
     }
  }
+printFlavorReport(flavorTotal);
